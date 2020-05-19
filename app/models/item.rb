b6c820d685cc7bcd1a89c793c56d6e6c723a0989
Ubuntu <54514850+Sholorcain@users.noreply.github.com>
2020-05-19 05:06:03 +0000

    final touches

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,6 @@
 class Item < ApplicationRecord
+  belongs_to :purchase_order
+  
   validates :id, uniqueness: true
   validates :name, presence: true, allow_blank: false
   validates :description, presence: true, length: { maximum: 500 }, allow_blank: false
