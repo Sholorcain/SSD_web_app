8c18f968cc1c1c8708be6f6db4eecc4c715538d0
Ubuntu <54514850+Sholorcain@users.noreply.github.com>
2020-05-19 02:31:46 +0000

    fixed bg image

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,6 @@
 class Item < ApplicationRecord
+  validates :id, uniqueness: true
+  validates :name, presence: true, allow_blank: false
+  validates :description, presence: true, length: { maximum: 500 }, allow_blank: false
+  validates :price, presence: true, allow_blank: false
 end
--- app/models/purchase_order.rb
+++ app/models/purchase_order.rb
@@ -1,2 +1,6 @@
 class PurchaseOrder < ApplicationRecord
+  validates :id, uniqueness: true
+  validates :date_created, presence: true, allow_blank: false
+  validates :quantity, presence: true, allow_blank: false
+  validates :total_price, presence: true, allow_blank: false
 end
